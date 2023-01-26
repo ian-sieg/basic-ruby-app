d91ed35decab330d5fea73c072e8b0321c3297dd
ian.moss <ianmsieg@gmail.com>
2023-01-25 21:44:59 -0600

    added basic user signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # GET /about
   root to:'main#index'
 
+  get "signup", to: 'registrations#new'
+  post "signup", to: 'registrations#create'
+
   get "about-us", to: 'about#index', as: :about
 end
