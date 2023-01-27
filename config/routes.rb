ab4f62b782f4f545762a87f8ef4efa2d00a6b3f9
ian.moss <ianmsieg@gmail.com>
2023-01-26 18:58:56 -0600

    added ability to reset password by email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # GET /about
   root to:'main#index'
+  get "about", to: 'about#index'
 
   get 'password', to: 'passwords#edit', as: :edit_password
   patch 'password', to: 'passwords#update'
@@ -12,5 +13,9 @@ Rails.application.routes.draw do
   post "sign_in", to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  get "about", to: 'about#index'
+  get 'password/reset', to: 'password_resets#new'
+  post 'password/reset', to: 'password_resets#create'
+  get 'password/reset/edit', to: 'password_resets#edit'
+  patch 'password/reset/edit', to: 'password_resets#update'
+
 end
